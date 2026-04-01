f4ccec05efbdb244f15a116fd8e314c1e1dac38d
Levi Lesches <levilesches@gmail.com>
2026-04-01 18:19:01 -0400

    Added Income.nextPayDay

--- lib/src/data/income.dart
+++ lib/src/data/income.dart
@@ -4,9 +4,11 @@ import "utils.dart";
 class Income {
   final Money annualSalary;
   final Money annualTaxes;
+  final DateTime firstPayDay;
   Income({
     required this.annualSalary,
     required this.annualTaxes,
+    required this.firstPayDay,
   });
 
   Money get annualIncome => annualSalary - annualTaxes;
@@ -16,9 +18,20 @@ class Income {
   Json toJson() => {
     "annualSalary": annualSalary.toJson(),
     "annualTaxes": annualTaxes.toJson(),
+    "firstPayDay": firstPayDay.toIso8601String(),
   };
 
   Income.fromJson(Json json) :
     annualSalary = Money.fromJson(json["annualSalary"]),
-    annualTaxes = Money.fromJson(json["annualTaxes"]);
+    annualTaxes = Money.fromJson(json["annualTaxes"]),
+    firstPayDay = DateTime.parse(json["firstPayDay"]);
+
+  DateTime get nextPayday {
+    final now = DateTime.now();
+    var date = firstPayDay;
+    while (date.isBefore(now)) {
+      date = date.add(const Duration(days: 14));
+    }
+    return date;
+  }
 }
